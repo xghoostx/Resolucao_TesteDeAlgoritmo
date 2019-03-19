c1e7e786ce90c63d3aa22ae947ded0a5532a191e
xghoostx <vasconcelosdeveloper@outlook.com>
2019-03-19 14:04:22 -0300

    correcao do algoritmo para calculos de numeros muito grandes

--- Ewerson_TesteAlgoritmos.js
+++ Ewerson_TesteAlgoritmos.js
@@ -1,23 +1,24 @@
 function createCheckDigit(membershipId) {
 
+    if (!membershipId||typeof membershipId === 'string' && !membershipId.trim()) console.log('Valor inválido');
+    if(membershipId < 0) console.log('Digite um valor positivo');
+
     let result = membershipId.split(''); // This function splits the array
     let resultInt = result.map(Number); // This function convert to integer
     let sum = resultInt.reduce((sum, resultInt) => sum + resultInt); // This function sum the array
+    let test = sum.toString().split(''); // This function convert to string
+    let validation = test.length;
+    
+    if(validation == 1) console.log(sum);
+    while(validation != 1){
 
-    if (sum >= 0 && sum  <= 9) {
-
-        console.log(sum);
-
-    } else {
-
-        let correct = sum.toString(); // This function convert to string
-        correct = correct.split('');
-        let correctInt = correct.map(Number);
-        final = correctInt.reduce((final, correctInt) => final + correctInt);
+        result = sum.toString().split(''); 
+        resultInt = result.map(Number); 
+        sum = resultInt.reduce((sum, resultInt) => sum + resultInt); 
+        test = sum.toString().split(''); 
+        validation = test.length;
         
-        console.log(final);
-
+        if(validation == 1) console.log(sum);
     }
 }
-
 createCheckDigit('55555');
